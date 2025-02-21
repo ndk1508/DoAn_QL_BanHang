5d15b229334b6d9bfbe9b2e6c0b96b3c29254bc2
ndk1508 <khoa223355@student.nctu.edu.vn>
2025-02-21 13:42:23 +0700

    update

--- Do_An_DotNet/UC_NhaCC.Designer.cs
+++ Do_An_DotNet/UC_NhaCC.Designer.cs
@@ -28,18 +28,211 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbThuongHieu = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSoDienThoai = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNhaPhanPhoi = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgvNhaPhanPhoi = new System.Windows.Forms.DataGridView();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNhaPhanPhoi)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbThuongHieu);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtSoDienThoai);
+            this.groupBox1.Controls.Add(this.btnLuu);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtDiaChi);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtNhaPhanPhoi);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(34, 83);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(824, 220);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin nhà phân phối ";
+            // 
+            // cbThuongHieu
+            // 
+            this.cbThuongHieu.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbThuongHieu.FormattingEnabled = true;
+            this.cbThuongHieu.Location = new System.Drawing.Point(550, 94);
+            this.cbThuongHieu.Margin = new System.Windows.Forms.Padding(2);
+            this.cbThuongHieu.Name = "cbThuongHieu";
+            this.cbThuongHieu.Size = new System.Drawing.Size(223, 26);
+            this.cbThuongHieu.TabIndex = 9;
+            this.cbThuongHieu.SelectedIndexChanged += new System.EventHandler(this.cbThuongHieu_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(430, 100);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Mã thương hiệu:";
+            // 
+            // txtSoDienThoai
+            // 
+            this.txtSoDienThoai.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoDienThoai.Location = new System.Drawing.Point(550, 49);
+            this.txtSoDienThoai.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSoDienThoai.Name = "txtSoDienThoai";
+            this.txtSoDienThoai.Size = new System.Drawing.Size(223, 24);
+            this.txtSoDienThoai.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(430, 54);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Số điện thoại:";
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDiaChi.Location = new System.Drawing.Point(158, 99);
+            this.txtDiaChi.Margin = new System.Windows.Forms.Padding(2);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(223, 24);
+            this.txtDiaChi.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 105);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Địa chỉ:";
+            // 
+            // txtNhaPhanPhoi
+            // 
+            this.txtNhaPhanPhoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNhaPhanPhoi.Location = new System.Drawing.Point(158, 50);
+            this.txtNhaPhanPhoi.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNhaPhanPhoi.Name = "txtNhaPhanPhoi";
+            this.txtNhaPhanPhoi.Size = new System.Drawing.Size(223, 24);
+            this.txtNhaPhanPhoi.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 52);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nhà phân phối:";
+            // 
+            // dgvNhaPhanPhoi
+            // 
+            this.dgvNhaPhanPhoi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNhaPhanPhoi.Location = new System.Drawing.Point(34, 307);
+            this.dgvNhaPhanPhoi.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvNhaPhanPhoi.Name = "dgvNhaPhanPhoi";
+            this.dgvNhaPhanPhoi.RowHeadersWidth = 51;
+            this.dgvNhaPhanPhoi.RowTemplate.Height = 24;
+            this.dgvNhaPhanPhoi.Size = new System.Drawing.Size(824, 365);
+            this.dgvNhaPhanPhoi.TabIndex = 8;
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLuu.Image = global::Do_An_DotNet.Properties.Resources.add__1_;
+            this.btnLuu.Location = new System.Drawing.Point(648, 159);
+            this.btnLuu.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(125, 42);
+            this.btnLuu.TabIndex = 11;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLuu.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click_1);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.ForeColor = System.Drawing.Color.Red;
+            this.btnXoa.Image = global::Do_An_DotNet.Properties.Resources.delete__1_;
+            this.btnXoa.Location = new System.Drawing.Point(733, 690);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(2);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(125, 46);
+            this.btnXoa.TabIndex = 10;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(336, 17);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(249, 33);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "NHÀ CUNG CẤP";
             // 
             // UC_NhaCC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvNhaPhanPhoi);
+            this.Controls.Add(this.btnXoa);
             this.Name = "UC_NhaCC";
             this.Size = new System.Drawing.Size(892, 761);
+            this.Load += new System.EventHandler(this.UC_NhaCC_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNhaPhanPhoi)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbThuongHieu;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSoDienThoai;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNhaPhanPhoi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgvNhaPhanPhoi;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Label label5;
     }
 }
--- Do_An_DotNet/UC_NhaCC.cs
+++ Do_An_DotNet/UC_NhaCC.cs
@@ -7,14 +7,221 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Do_An_DotNet
 {
     public partial class UC_NhaCC : UserControl
     {
+        private string connectionString = "Data Source=DESKTOP-N5BJBSG;Initial Catalog=QL_BanHang;Integrated Security=True;Encrypt=False;";
         public UC_NhaCC()
         {
             InitializeComponent();
+        }
+        // Hàm kết nối SQL Server
+        private SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+
+        // Load dữ liệu thương hiệu vào ComboBox
+        private void LoadThuongHieu()
+        {
+            using (SqlConnection conn = GetConnection())
+            {
+                string query = "SELECT MA_TH, TEN_TH FROM THUONGHIEU";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                cbThuongHieu.DataSource = dt;
+                cbThuongHieu.DisplayMember = "TEN_TH"; // Hiển thị tên thương hiệu
+                cbThuongHieu.ValueMember = "MA_TH";    // Lưu giá trị mã thương hiệu
+            }
+        }
+
+        // Load dữ liệu nhà phân phối lên DataGridView
+        private void LoadNhaPhanPhoi()
+        {
+            using (SqlConnection conn = GetConnection())
+            {
+                string query = @"
+                    SELECT 
+                        NSX.MA_NSX, 
+                        NSX.MA_TH, 
+                        TH.TEN_TH, 
+                        NSX.TEN_NSX, 
+                        NSX.SDT_NSX, 
+                        NSX.DIACHI_NSX
+                    FROM NHA_SAN_XUAT NSX
+                    JOIN THUONGHIEU TH ON NSX.MA_TH = TH.MA_TH";
+
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dgvNhaPhanPhoi.DataSource = dt;
+            }
+        }
+
+        // Reset lại các ô nhập
+        private void ResetFields()
+        {
+            txtNhaPhanPhoi.Clear();
+            txtDiaChi.Clear();
+            txtSoDienThoai.Clear();
+            cbThuongHieu.SelectedIndex = 0;
+        }
+        private void LoadNhaSanXuat()
+        {
+            using (SqlConnection conn = GetConnection())
+            {
+                string query = "SELECT MA_NSX, TEN_NSX FROM NHA_SAN_XUAT"; // Đảm bảo tên cột đúng
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dgvNhaPhanPhoi.DataSource = dt; // Đảm bảo dgvNhaSanXuat hiển thị đúng danh sách
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra có dòng nào được chọn không
+            if (dgvNhaPhanPhoi.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Vui lòng chọn nhà sản xuất cần xóa!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Lấy mã NSX từ DataGridView
+            int maNSX;
+            try
+            {
+                maNSX = Convert.ToInt32(dgvNhaPhanPhoi.SelectedRows[0].Cells["MA_NSX"].Value);
+            }
+            catch
+            {
+                MessageBox.Show("Không tìm thấy cột MA_NSX trong DataGridView!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Xác nhận xóa
+            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn xóa nhà sản xuất này?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.No)
+            {
+                return;
+            }
+
+            using (SqlConnection conn = GetConnection())
+            {
+                conn.Open();
+
+                try
+                {
+                    // Xóa nhà sản xuất khỏi bảng NHA_SAN_XUAT
+                    string deleteQuery = "DELETE FROM NHA_SAN_XUAT WHERE MA_NSX = @MA_NSX";
+                    SqlCommand cmd = new SqlCommand(deleteQuery, conn);
+                    cmd.Parameters.AddWithValue("@MA_NSX", maNSX);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Xóa thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        LoadNhaSanXuat(); // Cập nhật danh sách
+                    }
+                    else
+                    {
+                        MessageBox.Show("Không tìm thấy nhà sản xuất cần xóa!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi xóa: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private void cbThuongHieu_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UC_NhaCC_Load(object sender, EventArgs e)
+        {
+            LoadThuongHieu();  // Load danh sách thương hiệu vào ComboBox
+            LoadNhaPhanPhoi();  // Load danh sách nhà phân phối vào DataGridView
+            LoadNhaSanXuat();
+        }
+
+        private void btnLuu_Click_1(object sender, EventArgs e)
+        {
+            string tenNPP = txtNhaPhanPhoi.Text.Trim();
+            string diaChi = txtDiaChi.Text.Trim();
+            string soDienThoai = txtSoDienThoai.Text.Trim();
+            int maTH;
+
+            // Kiểm tra thông tin nhập vào
+            if (string.IsNullOrEmpty(tenNPP) || string.IsNullOrEmpty(diaChi) || string.IsNullOrEmpty(soDienThoai))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Lấy giá trị MA_TH từ ComboBox
+            if (!int.TryParse(cbThuongHieu.SelectedValue.ToString(), out maTH))
+            {
+                MessageBox.Show("Thương hiệu không hợp lệ!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            using (SqlConnection conn = GetConnection())
+            {
+                conn.Open();
+
+                // Kiểm tra xem MA_TH có tồn tại trong bảng THUONGHIEU không
+                string checkQuery = "SELECT COUNT(*) FROM THUONGHIEU WHERE MA_TH = @MA_TH";
+                SqlCommand checkCmd = new SqlCommand(checkQuery, conn);
+                checkCmd.Parameters.AddWithValue("@MA_TH", maTH);
+
+                int count = (int)checkCmd.ExecuteScalar();
+                if (count == 0)
+                {
+                    MessageBox.Show("Mã thương hiệu không tồn tại!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    conn.Close();
+                    return;
+                }
+
+                // Thêm nhà phân phối mới
+                string query = @"
+                INSERT INTO NHA_SAN_XUAT (MA_TH, TEN_NSX, DIACHI_NSX, SDT_NSX)
+                VALUES (@MA_TH, @TEN_NSX, @DIACHI_NSX, @SDT_NSX)";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@MA_TH", maTH);
+                cmd.Parameters.AddWithValue("@TEN_NSX", tenNPP);
+                cmd.Parameters.AddWithValue("@DIACHI_NSX", diaChi);
+                cmd.Parameters.AddWithValue("@SDT_NSX", soDienThoai);
+
+                int result = cmd.ExecuteNonQuery();
+                conn.Close();
+
+                if (result > 0)
+                {
+                    MessageBox.Show("Thêm nhà phân phối thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadNhaPhanPhoi(); // Cập nhật lại DataGridView
+                    ResetFields(); // Xóa các ô nhập sau khi lưu thành công
+                }
+                else
+                {
+                    MessageBox.Show("Thêm thất bại!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
